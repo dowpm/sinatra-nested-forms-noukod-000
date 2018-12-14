93b2df279e099a29cf0f5e507151371e87c21600
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-14 17:27:22 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def initialize name, type, booty
+    @name, @type, @booty = name, type, booty
+    SHIP << self
+  end
+
+  def self.all
+    SHIP
+  end
+
+  def self.clear
+    SHIP.clear
+  end
 end
