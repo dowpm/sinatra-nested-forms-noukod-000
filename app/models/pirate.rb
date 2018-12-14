56553e4cfad54e7ee65ce7ecce18c9aa12fc45fc
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-14 19:09:53 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize param
+    @name, @weight, @height = param[:name], param[:weight], param[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  SHIP = []
+
+  def initialize ship
+    @name, @type, @booty = ship[:name], ship[:type], ship[:booty]
+    SHIP << self
+  end
+
+  def self.all
+    SHIP
+  end
+
+  def self.clear
+    SHIP.clear
+  end
 end
